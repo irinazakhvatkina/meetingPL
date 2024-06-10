bb4a44ea4f3df137db14e66dacb12e3cb933964d
Irina Zakhvatkina <irinazakhvatkina2307@gmail.com>
2024-06-10 17:35:39 +0500

    Completed the task2

--- Program.cs
+++ Program.cs
@@ -1,34 +1,61 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
-bool multiplicity (int x)
-{
-    if (x % 7 == 0 &&  x % 23 == 0)
-    {
-        return true;
-    }
-    return false;
-}
+// bool multiplicity (int x)
+// {
+//     if (x % 7 == 0 &&  x % 23 == 0)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
 
-void Result (int x)
-{
-    if (multiplicity (x))
-    {
-        Console.WriteLine ("да");
-    }
-    else
-    {
-        Console.WriteLine ($"Данное число не кратно 7 и 23, {x % 7 == 0 &&  x % 23 == 0}");
-    }
+// void Result (int x)
+// {
+//     if (multiplicity (x))
+//     {
+//         Console.WriteLine ("да");
+//     }
+//     else
+//     {
+//         Console.WriteLine ($"Данное число не кратно 7 и 23, {x % 7 == 0 &&  x % 23 == 0}");
+//     }
 
-}
+// }
 
-Console.WriteLine("Input first number:   ");
-int x = Convert.ToInt32(Console.ReadLine());
-Result (x);
+// Console.WriteLine("Input first number:   ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Result (x);
 
 // Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
 
+void Result (int x, int y)
+{
+    if (x>0 && y>0)
+    {
+        Console.WriteLine ("I");
+    }else 
+    if(x<0 && y<0)
+    {
+        Console.WriteLine ("III");
+    }else 
+    if(x<0 && y>0)
+    {
+        Console.WriteLine ("IV");
+    }else 
+    if(x>0 && y<0)
+    {
+        Console.WriteLine ("II");
+    }else
+    {
+        Console.WriteLine ("Точка не лежит ни в одной из плоскастей.");
+    }
+}
 
+Console.WriteLine("Input X number:   ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Y umber:  ");
+int y = Convert.ToInt32(Console.ReadLine());
+Result (x,y);
 
 // Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.dotnet
 
